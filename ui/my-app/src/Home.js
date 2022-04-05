34a5a62e2eb0775432cac8d7ba1d1dbc2ecd1d09
zaverijay <zaverijay2001@gmail.com>
2022-04-05 21:33:55 +0530

    Full-stack project

---
+++ ui/my-app/src/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h3 style={{bgcolor: "red"}}>This is Home page</h3>
+            </div>
+        )
+    }
+}
